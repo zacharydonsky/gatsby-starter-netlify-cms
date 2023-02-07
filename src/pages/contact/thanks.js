9ede8311b08523f5a5a763063697372b7bee8d2d
Zach Donsky <zach@cortex-design.com>
2023-02-07 15:35:58 -0500

    Added a "subscribe to newsletter" modal popup to volunteer and contact page. Created an IFrameModalButton component to accomidate this feature. Fixed "thank-you" messages for volunteer and contact form submissions.

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Layout from "../../components/Layout";
+import React from 'react';
+import Layout from '../../components/Layout';
 
 // eslint-disable-next-line
 export default () => (
@@ -8,7 +8,7 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>Thanks for contacting HATS!</p>
         </div>
       </div>
     </section>
--- src/pages/volunteer/index.js
+++ src/pages/volunteer/index.js
@@ -1,35 +1,48 @@
-import * as React from "react";
-import { navigate } from "gatsby-link";
-import Layout from "../../components/Layout";
-import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import * as React from 'react';
+import { navigate } from 'gatsby-link';
+import Layout from '../../components/Layout';
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react';
+import IFrameModalButton from '../../components/iframe_modal_button';
+
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 }
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isValidated: false };
+    this.state = { isValidated: false, is_active: false };
   }
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  setModalState() {
+    this.setState({
+      is_active: (this.state.is_active = !this.state.is_active),
+      is_active_attribute: (this.state.is_active_attribute = this.state
+        .is_active
+        ? 'is-active'
+        : ''),
+    });
+    this.setModalState = this.setModalState.bind(this);
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        "form-name": form.getAttribute("name"),
+        'form-name': form.getAttribute('name'),
         ...this.state,
       }),
     })
-      .then(() => navigate(form.getAttribute("action")))
+      .then(() => navigate(form.getAttribute('action')))
       .catch((error) => alert(error));
   };
 
@@ -39,7 +52,7 @@ export default class Index extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>Contact</h1>
+              <h1>Volunteer</h1>
               <form
                 name="contact"
                 method="post"
@@ -52,69 +65,71 @@ export default class Index extends React.Component {
                 <input type="hidden" name="form-name" value="contact" />
                 <div hidden>
                   <label>
-                    Don’t fill this out:{" "}
+                    Don’t fill this out:{' '}
                     <input name="bot-field" onChange={this.handleChange} />
                   </label>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"name"}>
+                  <label className="label" htmlFor={'name'}>
                     Your name
                   </label>
                   <div className="control">
                     <input
                       className="input"
-                      type={"text"}
-                      name={"name"}
+                      type={'text'}
+                      name={'name'}
                       onChange={this.handleChange}
-                      id={"name"}
+                      id={'name'}
                       required={true}
                     />
                   </div>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"email"}>
+                  <label className="label" htmlFor={'email'}>
                     Email
                   </label>
                   <div className="control">
                     <input
                       className="input"
-                      type={"email"}
-                      name={"email"}
+                      type={'email'}
+                      name={'email'}
                       onChange={this.handleChange}
-                      id={"email"}
+                      id={'email'}
                       required={true}
                     />
                   </div>
                 </div>
                 <div className="field">
-                  <label className="label" htmlFor={"message"}>
+                  <label className="label" htmlFor={'message'}>
                     Message
                   </label>
                   <div className="control">
                     <textarea
                       className="textarea"
-                      name={"message"}
+                      name={'message'}
                       onChange={this.handleChange}
-                      id={"message"}
+                      id={'message'}
                       required={true}
                     />
                   </div>
                 </div>
-                <div className="field" >
-                    <label className="label" htmlFor={"newsletter"}>
-                        I consent to be contacted via newsletter as well  --> 
-                      <input
-                        type="checkbox"
-                        id={"newsletter"}
-                        name="newsletter"
-                        onChange={this.handleChange}
-                      />
-                    </label>
-                </div>
+
                 <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
+                  <nav className="navbar">
+                    <div className="navbar-start">
+                      <div className="navbar-item">
+                        <IFrameModalButton
+                          text="Subscribe To Our Newsletter"
+                          url="https://hamiltonpoverty.us14.list-manage.com/subscribe?u=d61456455bd43a35f1c9a6677&id=2d18eac361"
+                        />
+                      </div>
+                      <div className="navbar-item">
+                        <button className="button is-link" type="submit">
+                          Send
+                        </button>
+                      </div>
+                    </div>
+                  </nav>
                 </div>
               </form>
             </div>
