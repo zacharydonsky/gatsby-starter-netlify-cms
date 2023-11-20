51785df391671d4ae77f213d0d08dad28e457022
Zach Donsky <zach@cortex-design.com>
2023-11-19 21:57:04 -0500

    updated contact page to just list the two contact emails for volunteer & media stuff.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -41,82 +41,29 @@ export default class Index extends React.Component {
           <div className="container">
             <div className="content">
               <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={this.handleChange}
-                      id={'name'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={this.handleChange}
-                      id={'email'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={this.handleChange}
-                      id={'message'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <nav className="navbar">
-                    <div className="navbar-item">
-                      <IFrameModalButton
-                        text="Subscribe To Our Newsletter"
-                        url="https://hamiltonpoverty.us14.list-manage.com/subscribe?u=d61456455bd43a35f1c9a6677&id=2d18eac361"
-                      />
+              <div>
+                <div className="box">
+                  <div className="block">
+                    <div className="content">
+                      For <b>General</b> and <b>Volunteer</b> Inquiries, please
+                      get in touch with...
                     </div>
-                    <div className="navbar-item">
-                      <button className="button is-link" type="submit">
-                        Send
-                      </button>
+
+                    <div class="button is-medium is-link">
+                      info@hamiltontinyshelters.ca
                     </div>
-                  </nav>
+                  </div>
+                  <div className="block">
+                    <div className="content">
+                      For <b>Public Relations</b>, <b>Media Inquiries</b>, or{' '}
+                      <b>Events</b>, please get in touch with...
+                    </div>
+                    <div class="button is-medium is-light is-link">
+                      communications@hamiltontinyshelters.ca
+                    </div>
+                  </div>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </section>
